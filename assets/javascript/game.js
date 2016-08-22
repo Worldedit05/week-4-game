0e8b4004a8245ce15d801928bfdadb57e6201b21
Aaron Lee <worldedit05@gmail.com>
2016-08-22 01:02:00 -0500

    Code outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// Possible variables: 
+// character count
+// attack power
+// health points
+// counter attack power
+
+// Function to start the game
+
+function gameStart (){
+
+}
+
+// function to set the player character if 
+// no other character has been selected
+// i.e. "first" selection. Then set the 
+// oppenenant if it is not the first selection
+
+// Might need a function that resolves combat
+// and adjusts stats for the "grind" also 
+// needs to start listening again for the
+// next combat
+
+function combat (){
+
+}
+
+// Listen for the user clicks to capture 
+// the selection of a character and move 
+// it to the appropriate area 
+
+$("#character").click();
+
+// Attack button calls a combat function
+
+$("attack").click();
