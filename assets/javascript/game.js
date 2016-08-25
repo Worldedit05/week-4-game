3f64172362171326c30ab11da2e2a8fc1c24c705
Aaron Lee <worldedit05@gmail.com>
2016-08-25 15:12:48 -0500

    Added stats to the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,39 @@
 $(document).ready(function() {
 
     var stats = {
-        character1: {
-            attack: 8,
-            HP: 150,
-            counterAttack: 25
+        "character1": {
+            "attack": 8,
+            "HP": 150,
+            "counterAttack": 25
         },
-        character2: {
-            attack: 8,
-            HP: 120,
-            counterAttack: 20
+        "character2": {
+            "attack": 8,
+            "HP": 120,
+            "counterAttack": 20
         },
-        character3: {
-            attack: 8,
-            HP: 100,
-            counterAttack: 30
+        "character3": {
+            "attack": 8,
+            "HP": 100,
+            "counterAttack": 30
         },
-        character4: {
-            attack: 8,
-            HP: 140,
-            counterAttack: 22
+        "character4": {
+            "attack": 8,
+            "HP": 140,
+            "counterAttack": 22
         },
-        character5: {
-            attack: 8,
-            HP: 130,
-            counterAttack: 18
+        "character5": {
+            "attack": 8,
+            "HP": 130,
+            "counterAttack": 18
         }
     }
 
-
-    // // function to set the player character if 
-    // // no other character has been selected
-    // // i.e. "first" selection. Then set the 
-    // // oppenenant if it is not the first selection
-
-    // function selectCharacter() {
-
-    //     if (heroSet < 0 || defenderSet < 0) {
-
-
-    //             if (heroSet < 0) {
-
-    //                 $('#user').append($(this).addClass('hero'));
-    //                	$('#enemies').append($('.starter').children());
-    //                	heroSet++;
-
-    //             } else if (heroSet >= 0 && defenderSet < 0) {
-
-    //                 $('#computer').append($(this).addClass('defender'));
-    //                 defenderSet++;
-    //                 console.log(stats[$('.defender').attr('id')]);
-    //                 return stats[$('.defender').attr('id')]
-    //             }
-    //     }
-    // }
-
+    $('#character1').attr('data-attack', stats.character1.attack).attr('data-HP', stats.character1.HP).attr('data-counterAttack', stats.character1.counterAttack);
+    $('#character2').attr('data-attack', stats.character2.attack).attr('data-HP', stats.character2.HP).attr('data-counterAttack', stats.character2.counterAttack);
+    $('#character3').attr('data-attack', stats.character3.attack).attr('data-HP', stats.character3.HP).attr('data-counterAttack', stats.character3.counterAttack);
+    $('#character4').attr('data-attack', stats.character4.attack).attr('data-HP', stats.character4.HP).attr('data-counterAttack', stats.character4.counterAttack);
+    $('#character5').attr('data-attack', stats.character5.attack).attr('data-HP', stats.character5.HP).attr('data-counterAttack', stats.character5.counterAttack);
+    
     // Might need a function that resolves combat
     // and adjusts stats for the "grind" also 
     // needs to start listening again for the
